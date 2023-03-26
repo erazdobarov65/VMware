7884d08f8403ddf259a33c442d69f6ed5f00cbe4
erazdobarov65 <121334680+erazdobarov65@users.noreply.github.com>
2023-03-26 10:16:27 +0700

    Update vmware_lost_vm_info.py

--- Lost_VM/vmware_lost_vm_info.py
+++ Lost_VM/vmware_lost_vm_info.py
@@ -3,7 +3,6 @@ import ssl, csv, smtplib, paramiko, atexit, pyVim, os, re, dns.resolver
 from os.path import basename
 from pyVmomi import vim
 from pyVim.connect import SmartConnect, Disconnect
-#from tools import pchelper, service_instance
 
 # Enter you connection details
 VC_HOSTs = ["esxi_host1_name.xxx.xx", "esxi_host2_name.xxx.xx"] #array of hypervisor ESXi hosts
@@ -65,7 +64,6 @@ def DNSresolve(vm_name):
     except dns.exception.DNSException:
         dns_resolv_status = "error"
         dns_resolv_status = str(dns_resolv_status)
-                    #print(dns_resolv_status)
     else:
         dns_resolv_status = "ok"
     return dns_resolv_status
@@ -73,13 +71,13 @@ def DNSresolve(vm_name):
 
 def main():
     #Clean up csv file
-    cloneinfo = "../csv/cloneinfovmw.csv" #Save csv file inside 'csv' folder
+    csvinfo = "../csv/csvinfovmw.csv" #Save csv file inside 'csv' folder
     clearCSV(cloneinfo)
 
-    #VMW_NAME = 'esx'
+    #Hard code name of ESXi Pool
     VMW_POOL = 'bft-esxi-pool'
 
-    for VC_HOST in VC_HOSTs: #Перебираем все ESXi из массива
+    for VC_HOST in VC_HOSTs: #Loop the array of ESXi hosts
         service_instance = vc_connect(VC_HOST)
         atexit.register(Disconnect, service_instance)
         content = service_instance.RetrieveContent()
@@ -93,15 +91,12 @@ def main():
                 for vm in vmList:
                     summary = vm.summary
                     vm_name = summary.config.name
-                    powerState = vm.runtime.powerState
-                    if powerState == "poweredOff":
-                        dns_resolv_state = DNSresolve(vm_name)
-
-                        if dns_resolv_state == "error":
-                          
-                        #Записываем данные по ВМ в CSV
-                            vm_param_array = VmInfo(vm, VMW_POOL)
-                            appendCSV(vm_param_array,cloneinfo)  
+                    dns_resolv_state = DNSresolve(vm_name)
+                    #Try to resolve VM in DNS
+                    if dns_resolv_state == "error":
+                    #If VM does not resolve DNS add its details into csv file
+                        vm_param_array = VmInfo(vm, VMW_POOL)
+                        appendCSV(vm_param_array,cloneinfo)  
 
 
 if __name__ == '__main__':
