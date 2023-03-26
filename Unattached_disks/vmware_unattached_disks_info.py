b347eca339a93dcb09e78e753158ca51da688644
erazdobarov65 <121334680+erazdobarov65@users.noreply.github.com>
2023-03-26 10:44:48 +0700

    Update vmware_unattached_disks_info.py

--- Unattached_disks/vmware_unattached_disks_info.py
+++ Unattached_disks/vmware_unattached_disks_info.py
@@ -3,34 +3,30 @@ import ssl, csv, smtplib, atexit, pyVim, os, base64, urllib.request, urllib.pars
 from os.path import basename
 from pyVmomi import vim, vmodl
 from pyVim.connect import SmartConnect, Disconnect
-#from tools import pchelper, service_instance
 from pyVim.task import WaitForTask
 
-#
-VC_ALLHOSTs = ["hyp07.ftc.ru", "hyp08.ftc.ru", "hyp09.ftc.ru", "hyp10.ftc.ru", "hyp16.ftc.ru"]
-VC_USER        = "ops" #Rundeck user
-VC_PASS        = "2019@VMops!User"
+# Enter you connection details
+VC_HOSTs = ["esxi_host1_name.xxx.xx", "esxi_host2_name.xxx.xx"] #array of hypervisor ESXi hosts
+VC_USER        = "ESXi_user" #Read-only user
+VC_PASS        = "ESI_password"
 
-## Функция соединения с ESXi
+#Function to establish connection with ESXi host
 def vc_connect(VC_HOST):
     context = ssl._create_unverified_context()
     si = None
     si = SmartConnect(host=VC_HOST, port=443, user=VC_USER, pwd=VC_PASS, sslContext=context)
     return si
 
-## Функция отключения от ESXi
+#Function to disconnect from ESXi host
 def vc_disconnect(service_instance):
     Disconnect(service_instance)
 
-
-
-# Получаем инфо по дискам, которые привязаны к ВМ
+#Get info about disks tha are attahched to VMs
 def find_vm_disk(vm, VC_HOST, VIRT_NAME, VMW_POOL):
     # VM_name
     summary = vm.summary
     virt_name = VIRT_NAME
     vmw_pool = VMW_POOL
-    #ext_data = vm.extensiondata
     vm_name = summary.config.name
     vm_date = vm.config.createDate.strftime("%Y-%m-%d %H:%M")
     #print(vm.config)
@@ -42,45 +38,34 @@ def find_vm_disk(vm, VC_HOST, VIRT_NAME, VMW_POOL):
     for device in devices:
         #print(device)
         if isinstance(device, vim.vm.device.VirtualDisk):
-            vm_disk = device.backing.fileName
-            if  vm_disk == "[BFT-G350-VMW01] ISOs/cobbler-boot.iso":
-               print(device)
             vm_disk_array.append(vm_disk)
     return vm_disk_array
 
-    #vm_param_array = [[virt_name, vmw_pool, vm_name, vm_date, vm_disk_summ]]
-    #return vm_param_array
 
-
-
-
-#Очищаем csv
+#Function to clean up csv
 def clearCSV(vmhostinforesources):
     filename = vmhostinforesources
     f = open(filename, "w+")
     f.close()
 
 
-#Добавляем записи в csv
+#Function to add entry into csv file
 def appendCSV(title, vmhostinforesources):
     with open(vmhostinforesources, 'a', newline='', encoding='utf-8') as csvfile:
         writer = csv.writer(csvfile, delimiter=';')
         writer.writerows(title)
 
 
-
-## Функция поиска дисков, которые не привязаны к ВМ
+#Function to search for disks that are not attched to VMs
 def find_all_disk(datacenters, VMW_POOL, diskinfo, vm_disks_array_host_all):
+    #Here we use DatastoreBrowser classes to find disks inside datastores
     queryDetailsDisk = vim.host.DatastoreBrowser.VmDiskQuery.Details()
     queryDetailsDisk.capacityKb = True
     queryDetailsDisk.diskType = True
-    #queryDetailsDisk.fileType = True
-    #queryDetailsDisk.fileSize = True
     queryDetailsDisk.hardwareVersion = True
     queryDetailsDisk.thin = True
     search_spec = vim.host.DatastoreBrowser.SearchSpec(query=[vim.host.DatastoreBrowser.VmDiskQuery(details=queryDetailsDisk)])
     
-    #all_disks_array = []
     for dc in datacenters:
         for ds in dc.datastore:
             #print(ds.summary)
@@ -96,23 +81,16 @@ def find_all_disk(datacenters, VMW_POOL, diskinfo, vm_disks_array_host_all):
                 ds_store_list_name_str = ds_store_list_name[1:-1]
                 for file in sub_folder.file:
                     ds_file = file.path
-                    #if ds_file == "0004fb00001200007c38bcdaa87a89af.vmdk":
-                    #    print(sub_folder)
                     ds_file_str = ds_file[:-5]
                     ds_file_size_kb = file.capacityKb /1024 /1024
                     ds_file_size_gb = round(ds_file_size_kb)
                     ds_file_disk_path = ds_store +'/'+ ds_file
-                    #print(ds_file_disk_path)
-                    #if ds_file_disk_path == "[BFT-G350-VMW01]/cobbler-boot-new.iso":
-                    #    print(ds_file_disk_path)     
                     if ds_file_disk_path not in vm_disks_array_host_all:
-                    #all_disks_array.append(ds_file_disk_path)
-                    #print(ds_file_size_gb)
                         disk_param_array = [[VMW_POOL, ds_store_list_name_str, ds_file_str, ds_file_size_gb]]
                         appendCSV(disk_param_array, diskinfo)
     return disk_param_array  
 
-## Функция поиска ISO дисков
+#Function to search for ISO disks
 def find_iso_disk(datacenters, VMW_POOL, diskinfo):
     queryFileDetails = vim.host.DatastoreBrowser.FileInfo.Details()
     #queryDetailsDisk.capacityKb = True
@@ -137,14 +115,12 @@ def find_iso_disk(datacenters, VMW_POOL, diskinfo):
                 ds_store = sub_folder.folderPath
                 #print(ds_store)
                 ds_store = str(ds_store)
-                if ds_store == "[BFT-G350-VMW01] ISOs":
+                if ds_store == "[DATASTORE_NAME] ISOs": #make sure to enter here your custom 'DATASTORE_NAME'. That is where you keep all ISO images
                     ds_store_list = ds_store.split()
                     ds_store_list_name = str(ds_store_list[0])
                     ds_store_list_name_str = ds_store_list_name[1:-1]
                     for file in sub_folder.file:
                         ds_file = file.path
-                        #if ds_file == "0004fb00001200007c38bcdaa87a89af.vmdk":
-                        #    print(sub_folder)
                         ds_file_str = ds_file
                         ds_file_size_kb = file.fileSize /1024 /1024
                         ds_file_size_gb = round(ds_file_size_kb)
@@ -156,17 +132,18 @@ def find_iso_disk(datacenters, VMW_POOL, diskinfo):
     return disk_param_array 
     
 def main():
-    #Очищаем CSV
-    diskinfo = "/u/ckdba/unix_service/Infotask/check_unattached_vm_disk/csv/diskinfo_vmw.csv"
-    diskiso = "/u/ckdba/unix_service/Infotask/check_unattached_vm_disk/csv/diskiso_vmw.csv"
+    #Clean up csv file
+    diskinfo = "../csv/diskinfo_vmw.csv" #save your csv file with disk info inside 'csv' folder
+    diskiso = "../csv/diskiso_vmw.csv" #save your csv file with ISO info inside 'csv' folder
+    #Clean up csv files
     clearCSV(diskinfo)
     clearCSV(diskiso)
-
-    VMW_NAME = 'esx'
+    
+    #Hard code VMware pool
     VMW_POOL = 'bft-esxi-pool'
 
     vm_disks_array_total_host_all =  []
-    for VC_HOST in VC_ALLHOSTs: #Перебираем все ESXi из массива
+    for VC_HOST in VC_ALLHOSTs: #Loop through all ESXi hosts in the array
         service_instance = vc_connect(VC_HOST)
         content = service_instance.RetrieveContent()
 
@@ -182,30 +159,27 @@ def main():
                     VC_HOSTStr = str(VC_HOST)
                     VC_HOSTStr = VC_HOSTStr[:-7]                     
                     vm_disks_array = find_vm_disk(vm, VC_HOSTStr, VMW_NAME, VMW_POOL)
-                    vm_disks_array_total_host += vm_disks_array # Формируем общий массив со всех ВМ
+                    vm_disks_array_total_host += vm_disks_array # Create common array from all VMs inside hypervizor
                     #print(vm_disks_array)
         #print(vm_disks_array_total)
-        vm_disks_array_total_host_all += vm_disks_array_total_host #Форируем общий массив со всех хостов
+        vm_disks_array_total_host_all += vm_disks_array_total_host #Create coomon array from all hypervizors
     vc_disconnect(service_instance)
 
-    for VC_HOST in VC_ALLHOSTs: #Перебираем все ESXi из массива
+    for VC_HOST in VC_ALLHOSTs: #Loop through all ESXI hosts in the array
         try:
-            VC_HOSTStr = str(VC_HOST)
-            VC_HOSTStr = VC_HOSTStr[:-7]  
             service_instance = vc_connect(VC_HOST)
-            #atexit.register(Disconnect, service_instance)
             content = service_instance.RetrieveContent()
             datacenters = content.rootFolder.childEntity
             #datastores = datacenter.datastore
             find_all_disk(datacenters, VMW_POOL, diskinfo, vm_disks_array_total_host_all)
             find_iso_disk(datacenters, VMW_POOL, diskiso)
-            if socket.gethostbyname(VC_HOST): # если удалось подключится к первому хосту, то не идем дальше по циклу, т.к. к каждому хосту подключен одинаковый набор датасторов
+            if socket.gethostbyname(VC_HOST): # In case we succeed to connect to the first ESXi host in the array, we exit the loop, since all hosts have inside one cluster hve the same set of datasores
                 exit()
             #print(all_disks_array)
         except socket.gaierror:
             print(f"Can not connect to {VC_HOST}")
         else:
-            continue # Если не удалось подключиться к первому хосту идем дальше по циклу
+            continue # If we fail to connect to the first ESXi host in the array we move to the next one
 
     vc_disconnect(service_instance)
     
